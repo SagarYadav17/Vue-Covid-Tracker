d1346856afe13a2c5396e9a6dbe26acf1ea727dc
Sagar Yadav <sagaryadav17@outlook.com>
2021-12-30 11:51:46 +0530

    first commit

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="grid md:grid-cols-2 gap-4">
+    <!-- Box 1 -->
+    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        {{ numberWithCommas(status.NewConfirmed) }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ numberWithCommas(status.TotalConfirmed) }}
+      </div>
+    </div>
+
+    <!-- Box 2 -->
+    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        {{ status.NewDeaths }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ status.TotalDeaths }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DataBoxes",
+  props: ["status"],
+  methods: {
+    numberWithCommas(number) {
+      return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+
+    <button
+      v-if="status.Country"
+      class="
+        bg-green-700
+        text-white
+        rounded
+        p-3
+        mt-5
+        mb-10
+        focused:outline-none
+        hover:bg-green-500
+      "
+      @click="clearCountryData"
+    >
+      Clear Country
+    </button>
+
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :status="status" />
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DataTitle from "@/components/DataTitle";
+import DataBoxes from "@/components/DataBoxes";
+import CountrySelect from "@/components/CountrySelect";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: { DataTitle, DataBoxes, CountrySelect },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      status: {},
+      countries: [],
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+
+    getCountryData(country) {
+      this.status = country;
+      this.title = country.Country;
+    },
+
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = "Global";
+      this.status = data.Global;
+      this.loading = false;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+
+    this.dataDate = data.Date;
+    this.status = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
+};
 </script>
